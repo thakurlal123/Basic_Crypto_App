2e5afe02042f2dafe9dbef0d2acc0d0939cab2f8
Thakurlal123 <erthakurlal15@gmai.com>
2023-10-08 22:08:41 +0530

    This is basic Crypto App to purpose of show ratting

---
+++ app/src/main/java/com/example/cryptoapp/Modal.kt
@@ -0,0 +1,6 @@
+package com.example.cryptoapp
+
+class Modal (val name:String,val symbol:String,val price:String) {
+
+
+}
---
+++ app/src/main/java/com/example/cryptoapp/RvAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.cryptoapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.databinding.RvItemBinding
+
+class RvAdapter(val context: Context, var data: ArrayList<Modal>):RecyclerView.Adapter<RvAdapter.ViewHolder>(){
+
+    fun changeData(filterdata: ArrayList<Modal>) {
+        data = filterdata
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(val binding: RvItemBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view  = RvItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.name.text = data[position].name
+        holder.binding.price.text = data[position].price
+        holder.binding.symbol.text = data[position].symbol
+        setAnimation(holder.itemView)
+    }
+    override fun getItemCount(): Int {
+        return data.size
+    }
+    fun setAnimation(view: View){
+        val anim = AlphaAnimation(0.0f,1.0f)
+        anim.duration=1000
+        view.startAnimation(anim)
+
+    }
+
+
+}
