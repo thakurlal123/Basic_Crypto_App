558963e83c5a1d4068685491921c805eabe84576
Thakurlal123 <erthakurlal15@gmai.com>
2023-10-08 23:39:19 +0530

    add Splace Screen

---
+++ app/src/main/java/com/example/cryptoapp/SpActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cryptoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sp)
+
+        Handler(Looper.getMainLooper()).postDelayed(3000){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+}
